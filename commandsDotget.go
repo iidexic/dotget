87671269ed228e17e19afb3c300b631b42f6186c
iidexic <barrierloss@yahoo.com>
2025-05-05 19:44:12 -0700

    Continue writing out cli commands

--- commandsDotget.go
+++ commandsDotget.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/urfave/cli/v3"
 
-func commandInit() {
+func makeCommands() []*cli.Command {
 	cinit := &cli.Command{
 		Name:                   "init",
 		UseShortOptionHandling: true,
@@ -13,13 +13,25 @@ func commandInit() {
 		Category: "initialize",
 	}
 	cinit.Flags = []cli.Flag{
-		// WARNING: DIR/PATH FLAGS MAY NEED TO BE ARGUMENTS OR SUBCOMMANDS.
-		// I do not know if flags themselves support arguments like this?
-		// If not just add more usage details of init function with arg callout
+		// NOTE: can flags take arguments?
 		&cli.BoolFlag{Name: "envVar", Aliases: []string{"e"}, Usage: "store config location in an environment variable instead of file in exe dir"},
 		&cli.StringFlag{Name: "dir", Aliases: []string{"d"}, Usage: "specify a main directory to store dotget's config"},
 		&cli.StringFlag{Name: "path"},
 		&cli.BoolFlag{Name: "notEmpty", Aliases: []string{"n"}, Usage: "allow config to be placed in an existing non-empty directory"},
 		&cli.BoolFlag{Name: "ForceOverwriteConfig", Usage: "Force write config, will overwrite any other file at destination path"},
 	}
+
+	//# Command set
+	cset := &cli.Command{
+		Name:                   "set",
+		UseShortOptionHandling: true,
+		Commands: []*cli.Command{
+			{Name: "make", Usage: "Make a new set for app configs"},
+			{Name: "switch", Usage: "Switch to modifying/targeting a different set"},
+			{Name: "merge", Usage: "Merge two or more sets into one"},
+		},
+	}
+	cset.Flags = []cli.Flag{}
+	cmds := []*cli.Command{cinit, cset}
+	return cmds
 }
